ee9c85b49ce21929e8e2fb1804f1a44ac18b80b5
StacyChebet <staceychebet23@gmail.com>
2018-11-04 21:55:04 +0300

    add password propery to user object

--- user_data.py
+++ user_data.py
@@ -1,11 +1,12 @@
 import pyperclip
 class User:
     details_list = []
-    def __init__(self, first_name, last_name, phone_number, email):
+    def __init__(self, first_name, last_name, phone_number, email, password):
         self.first_name = first_name
         self.last_name = last_name
         self.phone_number = phone_number
         self.email = email
+        self.password = password
     def save_user(self):
         '''
         Saves new user details into details_list
