c7e886a913408dd9152ea06cf140ac9e664923a0
StacyChebet <staceychebet23@gmail.com>
2018-11-04 10:17:41 +0300

    Add new user object using the setUp function

--- user_test.py
+++ user_test.py
@@ -3,5 +3,15 @@ from user-data import User
 import pyperclip
 
 class TestUser(unittest.TestCase):
-    def tearDown(self):
-        User.details_list = []
+        def tearDown(self):
+            '''
+            Does clean up after each test has run
+            '''
+            User.details_list = []
+
+        def SetUp(self):
+            '''
+            Runs before each test case
+            '''
+            self.new_user = User(
+            "Stacy", "Chebet", "0712345678", "staceychebet23@gmail.com")
