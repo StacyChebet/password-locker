255babd83283b8ee339cec5a8485998a22ca5eee
StacyChebet <staceychebet23@gmail.com>
2018-11-04 19:04:32 +0300

    add the if__name__ == '__main__' function

--- user_test.py
+++ user_test.py
@@ -3,13 +3,13 @@ from user_data import User
 import pyperclip
 
 class TestUser(unittest.TestCase):
-        def tearDown(self):
-            '''
-            Does clean up after each test has run
-            '''
-            User.details_list = []
+        # def tearDown(self):
+        #     '''
+        #     Does clean up after each test has run
+        #     '''
+        #     User.details_list = []
 
-        def SetUp(self):
+        def setUp(self):
             '''
             Runs before each test case
             '''
@@ -24,3 +24,7 @@ class TestUser(unittest.TestCase):
             self.assertEqual(self.new_user.last_name, "Chebet")
             self.assertEqual(self.new_user.phone_number, "0712345678")
             self.assertEqual(self.new_user.email, "staceychebet23@gmail.com")
+
+
+if __name__ = '__main__':
+    unittest.main()
